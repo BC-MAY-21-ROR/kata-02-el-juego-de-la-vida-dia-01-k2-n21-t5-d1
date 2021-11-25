cef0aefac7b5262b3d1b155c28849883e6767071
Anisbeflo <abejarano0@ucol.mx>
2021-11-25 17:29:06 -0600

    Living cells generation
    - Calculation to get the middle position from the dimensions with height and width
    - Added bool var vivo for living cell
    - Added Matrix with var vivo true in the positions of the living cells
    
    Co-authored-by: Villaedi <ediberto.sanchez.villalobos@gmail.com>
    Co-authored-by: alanRazoMen <arazo@ucol.mx>
    Co-authored-by: DiegoSteve <diegosteve123321@gmail.com>

--- kata02.rb
+++ kata02.rb
@@ -1,12 +1,25 @@
+
 puts("Enter the height value: ")
 height=gets.chomp.to_i
 
 puts("Enter the width value: ")
 width=gets.chomp.to_i
 
-height.times do 
-  width.times do
-    print (".")
+matrix=Array.new(height){Array.new(width)}
+vivo=true
+mid_height = (height/2).to_i
+mid_width = (width/2).to_i
+
+
+height.times do |x|
+  width.times do |y|
+    
+    if (y==mid_width && x==mid_height)||(y==mid_width-1 && x==mid_height)||(y==mid_width && x==mid_height-1)
+      matrix[x][y]=vivo
+      print ("*")
+    else
+      print (".")
+    end
   end
   puts
 end
